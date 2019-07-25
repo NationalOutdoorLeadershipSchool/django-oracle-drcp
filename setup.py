3cc5c53d41b4ae30c0eb8a2b1d8c40028b6ccd4c
Steve Smith <stvsmth@gmail.com>
2019-07-25 12:46:53 -0600

    Reformat to our specs

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 
 setup(
-    name='django-oracle-drcp',
+    name="django-oracle-drcp",
     packages=find_packages(),
-    url='https://github.com/JohnPapps/django-oracle-drcp',
-    version='1.0.3',
-    description='A Django database backend for Oracle with DRCP',
-    author='John Papanastasiou',
-    author_email='s@johnpapps.com',
+    url="https://github.com/JohnPapps/django-oracle-drcp",
+    version="1.0.3",
+    description="A Django database backend for Oracle with DRCP",
+    author="John Papanastasiou",
+    author_email="s@johnpapps.com",
     classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Framework :: Django',
-        'License :: OSI Approved :: BSD License',
-        'Development Status :: 3 - Alpha',
-        'Operating System :: OS Independent',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Topic :: Database',
-    ]
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Topic :: Database",
+    ],
 )
-
